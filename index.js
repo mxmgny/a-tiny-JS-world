54c475cd62bdb791ee809b9ca3a8a51ed34f1760
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-01-24 15:09:08 +0200

    Deleted unnecessry comments

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
 let cat = {
     appearance: "cat",
     name: "Garfield",
@@ -45,26 +35,7 @@ let human = {
     friends:[dog['name'], "Robb", "Arya", "Bran", "Rickon"]
 };
 
-
 let arr = [cat, dog, human,cat_woman];
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
 
 for(let i = 0;i<arr.length;i++){
     let obj = arr[i];
